70326ebdde65b9dcff3cbbeac6050a0d586fd722
pachbrasil <pachbrasil@outlook.com>
2022-02-06 07:18:44 -0300

    Alterando 3o commit.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Sending to github directly by vscode IDE.
 // Testing with my account 
 
-console.log('Hello World of Github');
-console.log('2nd commit: '); 
-console.log('This is the second commit i try to use');
+
